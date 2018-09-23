a58d2f900260db2ce0f5d4ae946e1d848663132b
Adam Mazurkiewicz <adam.mazurkiewicz@ventum.com>
2018-09-23 14:29:07 +0200

    init commit

---
+++ app/src/main/java/com/adam/ipgetter/MainActivity.java
@@ -0,0 +1,15 @@
+package com.adam.ipgetter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ipgetter.adam.com.ipgetter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
